739b9cd6440a914859013e36941fc02fa83607c5
Gennaro Bosone <gennaro.bs@gmail.com>
2022-01-18 10:53:14 +0100

    up

--- node-index.js
+++ node-index.js
@@ -1,35 +1,37 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const fs = require('fs')
-   
-const gcode = fs.readFileSync('./data.gcode', { encoding:'utf8', flag:'r' })
+
+const gcode = fs.readFileSync('./data.gcode', { encoding: 'utf8', flag: 'r' })
 
 const commands = gcode.split('\n').map(command => command + '\r\n')
 
 const port = new SerialPort('/dev/ttyUSB0', {
-  baudRate: 115200
+	baudRate: 115200,
 })
-
+const commandsLength = commands.length
+let percentage = 100
 let sending = false
 const lineStream = port.pipe(new Readline())
 
-
 lineStream.on('data', function (data) {
-    console.log('data:', data)
+	const currentLength = commands.length
+	percentage = (100 - (currentLength / commandsLength) * 100).toFixed(2)
+	console.log(`data: ${data} | percentage: ${percentage}`)
 
-    dequeue()
-})  
+	dequeue()
+})
 
 function dequeue() {
-    if (!sending && commands.length > 0) {
-        const command = commands.shift()
-        sending = true
-        port.write(command, () => {
-            sending = false
-        })
-    }
+	if (!sending && commands.length > 0) {
+		const command = commands.shift()
+		sending = true
+		port.write(command, () => {
+			sending = false
+		})
+	}
 }
-  
-port.on('error', function(e) {
-    console.log('error: ', e.message)
+
+port.on('error', function (e) {
+	console.log('error: ', e.message)
 })
